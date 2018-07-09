6184b8fc9adf51460bc2ed0e05680b29cababedb
Schuman Zhang <sz@proadviser.com.au>
2018-07-09 22:12:45 +1000

    fixed video writer

--- video_writer.py
+++ video_writer.py
@@ -6,13 +6,13 @@ class VideoWriter(object):
         self.path = path
         self.size = size
         self.writer = cv2.VideoWriter(self.path, 
-                         cv2.VideoWriter_fourcc('M','J','P','G'), 
+                         cv2.VideoWriter_fourcc('F','M','P','4'), 
                          20.0, self.size, True)
 
     def __call__(self, frame):
         self.writer.write(frame)
 
-    def close():
+    def close(self):
         self.writer.release()
 
 
